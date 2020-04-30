fbea012dd27623c0de89d0a06b94a3ac6777b5f5
cesar vargas <djkav2009@gmail.com>
2020-04-29 20:45:03 -0500

    ajustes enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-const BASE_URL = 'https://mipropiedadhorizontal.com.co/api';
-const UPLOADS_CLIENTES = 'http://mipropiedadhorizontal.com.co/api/upload/documentosAdjuntos/';
-//const UPLOADS_CLIENTES = 'http://localhost:8080/api/upload/documentosAdjuntos/';
-//const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://mipropiedadhorizontal.com.co/apid';
 
 export const environment = {
-  production: true,
+  production: false,
   API_URL : BASE_URL + '/api',
-  UPLOADS_CLIENTES : UPLOADS_CLIENTES
+  UPLOADS_CLIENTES : BASE_URL +'/upload/documentosAdjuntos/' 
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.

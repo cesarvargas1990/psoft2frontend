8820ff3e1703eee30c956081039c1c11864ddd28
cesar vargas <djkav2009@gmail.com>
2020-04-29 20:46:12 -0500

    ajustes environments

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://mipropiedadhorizontal.com.co/apid';
+
+export const environment = {
+  production: false,
+  API_URL : BASE_URL + '/api',
+  UPLOADS_CLIENTES : BASE_URL +'/upload/documentosAdjuntos/' 
+};
---
+++ src/environments/environment.local.ts
@@ -0,0 +1,8 @@
+const BASE_URL = 'http://localhost:8080';
+
+export const environment = {
+  local: true,
+  production: false,
+  API_URL : BASE_URL + '/api',
+  UPLOADS_CLIENTES : BASE_URL + '/upload/documentosAdjuntos/'
+};

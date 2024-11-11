0db35c4a765bad1b38851edcd59d162a0a32361b
cesar vargas <cesara.vargas1990@gmail.com>
2024-11-10 19:16:09 -0500

    Se solucionan los siguientes errores:
    No se muestran fotos de cliente al editar
    No sale en la vista de editar cliente la firma actual ni para cambiarla
    Cuando se esta creando cliente y se toman fotos de documentos no muestra la foto capturada
    Cuando se esta creando cliente y se ingresa a editar solo sale una foto de las que se creo
    Cuando se selecciona un documento pdf al crear no se muestra bien la previsualizacion
    Cuadro de web cam se muestra en area diferente al editar cliente

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -3,6 +3,6 @@ const BASE_URL = 'http://localhost:8081';
 export const environment = {
   local: true,
   production: false,
-  API_URL : BASE_URL ,
-  UPLOADS_CLIENTES : BASE_URL + '/upload/documentosAdjuntos/'
+  API_URL: BASE_URL,
+  UPLOADS_CLIENTES: BASE_URL + '/upload/documentosAdjuntos/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://credisoft.co/api';
+const BASE_URL = 'https://mipropiedadhorizontal.com.co/api';
 
 export const environment = {
   production: false,
-  API_URL : BASE_URL + '',
-  UPLOADS_CLIENTES :  '/upload/documentosAdjuntos/' 
+  API_URL: BASE_URL + '',
+  UPLOADS_CLIENTES: BASE_URL + '/upload/documentosAdjuntos/'
 };

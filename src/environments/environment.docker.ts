d8a814ac17d79511bbd1c3586ce88c47eaeca4eb
cesar vargas <cesara.vargas1990@gmail.com>
2024-12-13 11:57:06 -0500

    se agrega dockerizacion de la app

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = 'http://localhost:8000';
+
+export const environment = {
+  local: true,
+  production: false,
+  API_URL: BASE_URL,
+  UPLOADS_CLIENTES: BASE_URL + '/documentosAdjuntos/',
+  GET_UPLOADS_PATH: BASE_URL + './documentosAdjuntos/'
+};

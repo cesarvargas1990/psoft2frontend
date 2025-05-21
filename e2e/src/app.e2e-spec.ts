26a8180ea22698aa4ecf51b280be21192c44432f
cesar vargas <cesara.vargas1990@gmail.com>
2025-05-21 17:36:14 -0500

    ajustes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('visualtechfront app is running!');
+    expect(page.getTitleText()).toEqual('psoft2frontend app is running!');
   });
 
   afterEach(async () => {

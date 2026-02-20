6bff6a644cc25ad470332049506587de0ce4de43
Cesar Augusto Vargas <cesara.vargas1990@gmail.com>
2026-02-20 12:20:40 -0500

    chore: update karma coverage reporter to version 2.2.0

--- karma.conf.js
+++ karma.conf.js
@@ -10,16 +10,16 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, './coverage/psoft2frontend'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'lcovonly' }, { type: 'text-summary' }]
     },
     reporters: ['progress'],
     port: 9876,
